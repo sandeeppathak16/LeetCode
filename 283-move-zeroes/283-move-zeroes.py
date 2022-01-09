24680b237a4d28a94ef49ea8ce5a180defbc01fd
sandeep pathak <spathak1698@gmail.com>
2022-01-09 18:13:09 +0530

    Time: 1227 ms (9.07%), Space: 14.5 MB (86.21%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        count = nums.count(0)
+        for i in range(0, count):
+            nums.remove(0)
+            nums.append(0)
