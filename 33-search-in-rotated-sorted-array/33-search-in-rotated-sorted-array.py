8b675080c5c8a2bf01224eeda1262e8afceb5fa6
sandeep pathak <spathak1698@gmail.com>
2022-02-09 14:21:40 +0530

    Time: 48 ms (57.39%), Space: 14.2 MB (91.98%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -2,8 +2,32 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         i = 0
         j = len(nums)-1
+        pivot = 0
         
-        if target not in nums:
+        def Bsearch(l, left, right, t):
+            
+            while left<=right:
+                mid = (left+right)//2
+                
+                if l[mid]==t:
+                    return mid
+                elif l[mid]>t:
+                    right = mid-1
+                else:
+                    left=mid+1
             return -1
+        
+        if nums[i]>nums[j]:
+            pivot = nums.index(max(nums))
+            a1 = Bsearch(nums, 0, pivot, target)
+            a2 = Bsearch(nums, pivot+1, j, target)
+            if a1!= -1:
+                return a1
+            elif a2!= -1:
+                return a2
+            else:
+                return -1
         else:
-            return nums.index(target) 
+            return Bsearch(nums, i, j, target)
+            
+                    
