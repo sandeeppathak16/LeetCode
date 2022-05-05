e91e7c73d9de5e973199d00b2bdb778efa1fe8a4
sandeep pathak <spathak1698@gmail.com>
2022-05-05 14:44:03 +0530

    Added solution - LeetHub

---
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def getPairsCount(self, arr, n, k):
+        
+        
+        # code here
+        d={}
+        count=0
+        
+        for ele in arr:
+            if k-ele in d:
+                count+=d[k-ele]
+            if ele not in d:
+                d[ele]=1
+            else:
+                d[ele]+=1
+        return count
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getPairsCount(arr, n, k)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
